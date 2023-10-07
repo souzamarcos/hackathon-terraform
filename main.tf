e03dc08920fa6abfd66a9538f48006c67ead4831
Marcos Souza <markin.mv.amaral@gmail.com>
2023-10-07 14:34:39 -0300

    Add multiple files

--- main.tf
+++ main.tf
@@ -27,39 +27,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-
-resource "random_password" "master"{
-  length           = 16
-  special          = true
-  override_special = "_!%^"
-}
-
-resource "aws_secretsmanager_secret" "password" {
-  name = "burger-db-password"
-}
-
-resource "aws_secretsmanager_secret_version" "password" {
-  secret_id = aws_secretsmanager_secret.password.id
-  secret_string = random_password.master.result
-}
-
-
-data "aws_secretsmanager_secret" "password" {
-  name = "burger-db-password"
-}
-
-data "aws_secretsmanager_secret_version" "password" {
-  secret_id = data.aws_secretsmanager_secret.password
-}
-
-resource "aws_db_instance" "default" {
-  allocated_storage             = 2
-  db_name                       = "mydb"
-  engine                        = "mysql"
-  engine_version                = "5.8"
-  instance_class                = "db.t3.micro"
-  username                      = "fiap"
-  password                      = data.aws_secretsmanager_secret_version.password
-  skip_final_snapshot           = true
-}
---
+++ rds.tf
@@ -0,0 +1,18 @@
+data "aws_secretsmanager_secret" "password" {
+  name = "burger-db-password"
+}
+
+data "aws_secretsmanager_secret_version" "password" {
+  secret_id = data.aws_secretsmanager_secret.password
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage             = 2
+  db_name                       = "mydb"
+  engine                        = "mysql"
+  engine_version                = "5.8"
+  instance_class                = "db.t3.micro"
+  username                      = "fiap"
+  password                      = data.aws_secretsmanager_secret_version.password
+  skip_final_snapshot           = true
+}
