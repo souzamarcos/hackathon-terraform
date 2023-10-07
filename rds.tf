927f72531e5a34d0b121252c8f6914b687a4b2f6
Marcos Souza <markin.mv.amaral@gmail.com>
2023-10-07 14:45:17 -0300

    Update rds config

--- rds.tf
+++ rds.tf
@@ -4,7 +4,7 @@ data "aws_secretsmanager_secret" "password" {
 }
 
 data "aws_secretsmanager_secret_version" "password" {
-  secret_id = data.aws_secretsmanager_secret.password
+  secret_id = data.aws_secretsmanager_secret.password.id
   depends_on = [aws_secretsmanager_secret.password]
 }
 
@@ -15,6 +15,6 @@ resource "aws_db_instance" "default" {
   engine_version                = "5.8"
   instance_class                = "db.t3.micro"
   username                      = "fiap"
-  password                      = data.aws_secretsmanager_secret_version.password
+  password                      = data.aws_secretsmanager_secret_version.password.secret_string
   skip_final_snapshot           = true
 }
