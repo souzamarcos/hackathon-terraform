bbb71dacc2d67d7f1038319f3f7c4d9f2a5a2402
Marcos Souza <markin.mv.amaral@gmail.com>
2023-10-07 14:08:36 -0300

    Update rds config

--- main.tf
+++ main.tf
@@ -36,10 +36,10 @@ resource "aws_db_instance" "default" {
   allocated_storage             = 2
   db_name                       = "mydb"
   engine                        = "mysql"
-  engine_version                = "5.7"
+  engine_version                = "5.8"
   instance_class                = "db.t3.micro"
   manage_master_user_password   = true
-  master_user_secret_kms_key_id = aws_kms_key.example.key_id
+  master_user_secret_kms_key_id = aws_kms_key.rds_kms_key.key_id
   username                      = "fiap"
   parameter_group_name          = "default.mysql5.7"
   skip_final_snapshot           = true
