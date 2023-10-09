a58b9a3cdecba06f49628b9d052ae2340a034b9d
Marcos Souza <markin.mv.amaral@gmail.com>
2023-10-09 13:45:03 -0300

    Update secrets config

--- secrets.tf
+++ secrets.tf
@@ -3,11 +3,6 @@ resource "aws_secretsmanager_secret" "db_burger" {
 }
 
 resource "aws_secretsmanager_secret_version" "db_burger" {
-  secret_id = aws_secretsmanager_secret.password.id
-  secret_string = random_password.master.result
-}
-
-resource "aws_secretsmanager_secret_version" "db_burger" {
   depends_on = [aws_db_instance.default]
   secret_id     = aws_secretsmanager_secret.db_burger.id
   secret_string = <<EOF
