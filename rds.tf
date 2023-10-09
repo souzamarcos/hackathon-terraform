8d5aa3b413d3e06bb4b1c7380cf71d95959ff8e5
Marcos Souza <markin.mv.amaral@gmail.com>
2023-10-09 13:28:57 -0300

    Set rds as public

--- rds.tf
+++ rds.tf
@@ -18,4 +18,5 @@ resource "aws_db_instance" "default" {
   username                      = var.username
   password                      = var.password
   parameter_group_name          = "default.mysql5.7"
+  publicly_accessible           = true
 }
