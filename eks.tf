8426fb0fa7a1048ac351af44572570e48a23c13a
Marcos Souza <markin.mv.amaral@gmail.com>
2023-10-14 17:24:45 -0300

    Alter cluster name

--- eks.tf
+++ eks.tf
@@ -9,7 +9,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  cluster_name = "education-eks-${random_string.suffix.result}"
+  cluster_name = "prod"
 }
 
 resource "random_string" "suffix" {
