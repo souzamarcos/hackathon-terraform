3dc124829412a55ae5291e441812b5359e847294
Marcos Souza <markin.mv.amaral@gmail.com>
2023-10-15 17:32:02 -0300

    Update node groups

--- eks.tf
+++ eks.tf
@@ -52,24 +52,13 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
+      instance_types = ["t2.micro"]
 
       min_size     = 1
       max_size     = 2
