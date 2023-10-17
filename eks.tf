5a9361c5b7b07b7e1d2e91e32c9b7b15fe970701
Marcos Souza <markin.mv.amaral@gmail.com>
2023-10-17 09:43:01 -0300

    Update instance_types

--- eks.tf
+++ eks.tf
@@ -58,7 +58,7 @@ module "eks" {
     one = {
       name = "node-group-1"
 
-      instance_types = ["t2.micro"]
+      instance_types = ["t3.small"]
 
       min_size     = 1
       max_size     = 2
