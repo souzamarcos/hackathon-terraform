7de533fcc0522476626da2c6787d87707e715f58
Marcos Souza <markin.mv.amaral@gmail.com>
2023-10-19 09:58:47 -0300

    Update rds subnet

--- rds.tf
+++ rds.tf
@@ -4,6 +4,15 @@ resource "random_password" "db_master_password"{
   override_special = "_!%^"
 }
 
+resource "aws_db_subnet_group" "default" {
+  name       = "rds-subnet-group"
+  subnet_ids = [aws_subnet.private_subnets.id]
+
+  tags = {
+    Name = "RDS subnet group"
+  }
+}
+
 resource "aws_db_instance" "default" {
   depends_on                    = [random_password.db_master_password]
   allocated_storage             = 5
@@ -16,4 +25,5 @@ resource "aws_db_instance" "default" {
   password                      = random_password.db_master_password.result
   parameter_group_name          = "default.mysql8.0"
   publicly_accessible           = true
+  db_subnet_group_name          = aws_db_subnet_group.default.name
 }
