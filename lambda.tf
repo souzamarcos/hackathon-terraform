2a929c60c42d10ca3ddb603b36a88f382f668b96
Guilherme Oliveira <ghvoliveira@gmail.com>
2023-10-28 13:09:45 -0300

    Lambda via terraform

---
+++ lambda.tf
@@ -0,0 +1,13 @@
+resource "aws_lambda_function" "java_lambda_function" {
+  runtime          = "java17"
+  filename      = "s3://fiap-burger/lambda-client-auth/lambda-tech-challenge-fast-food-1.0-SNAPSHOT-plain.jar"
+  source_code_hash = "${base64sha256(file("s3://fiap-burger/lambda-client-auth/lambda-tech-challenge-fast-food-1.0-SNAPSHOT-plain.jar"))}"
+  function_name = "java_lambda_function"
+
+  handler          = "handler.LambdaHandler"
+  timeout = 60
+  memory_size = 256
+  role             = "${aws_iam_role.iam_role_for_lambda.arn}"
+  depends_on   = ["aws_cloudwatch_log_group.log_group"]
+
+}
