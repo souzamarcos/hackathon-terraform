76e03660a9ec48d2554615ce873a7a4b0f97537f
Guilherme Oliveira <ghvoliveira@gmail.com>
2023-10-28 18:53:49 -0300

    Lambda via terraform - Mudando especificação do handler

--- lambda.tf
+++ lambda.tf
@@ -6,7 +6,7 @@ resource "aws_lambda_function" "java_lambda_function" {
   s3_key = "lambda-client-auth/app.zip"
   function_name = "java_lambda_function"
 
-  handler          = "handler.LambdaHandler"
+  handler          = "com.fiap.burger.handler.LambdaHandler::handleRequest"
   timeout = 60
   memory_size = 256
   role             = "${aws_iam_role.iam_role_for_lambda.arn}"
