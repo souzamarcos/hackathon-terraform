0e75511121b8081930b4666cea52ffb7e8798c91
Guilherme Oliveira <ghvoliveira@gmail.com>
2023-10-30 00:14:19 -0300

    dynamo.tf - Tentando comunicação dynamo.tf + lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "java_lambda_function" {
   runtime          = "java17"
   s3_bucket = "fiap-burger"
-  s3_key = "lambda-client-auth/lambda-client-authentication.zip"
+  s3_key = "lambda-client-auth/lambda-client-authentication-dynamo.zip"
   function_name = "java_lambda_function"
 
   handler          = "com.fiap.burger.handler.LambdaHandler::handleRequest"
@@ -10,6 +10,11 @@ resource "aws_lambda_function" "java_lambda_function" {
   role             = "${aws_iam_role.iam_role_for_lambda.arn}"
   depends_on   = ["aws_cloudwatch_log_group.log_group"]
 
+  environment {
+    variables = {
+      NOTES_TABLE = aws_dynamodb_table.tf_clients_table.name
+    }
+  }
 }
 
 resource "aws_lambda_permission" "java_lambda_function" {
