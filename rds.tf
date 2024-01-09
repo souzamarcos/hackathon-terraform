e76c1e2ee5fffb5f1edf977bda0b15261a3771bd
Marcos Souza <markin.mv.amaral@gmail.com>
2024-01-09 09:18:55 -0300

    feat: Add order and product rds

--- rds.tf
+++ rds.tf
@@ -1,19 +1,42 @@
-resource "random_password" "db_master_password"{
+resource "random_password" "db_product_password"{
   length           = 16
   special          = true
   override_special = "_!%^"
 }
 
-resource "aws_db_instance" "default" {
-  depends_on                    = [random_password.db_master_password]
+resource "random_password" "db_order_password"{
+  length           = 16
+  special          = true
+  override_special = "_!%^"
+}
+
+resource "aws_db_instance" "db_product" {
+  depends_on                    = [random_password.db_product_password]
   allocated_storage             = 5
-  db_name                       = "burger"
+  db_name                       = "product"
   engine                        = "mysql"
   engine_version                = "8.0.34"
   allow_major_version_upgrade   = true
   instance_class                = "db.t3.micro"
   username                      = "fiap"
-  password                      = random_password.db_master_password.result
+  password                      = random_password.db_product_password.result
+  parameter_group_name          = "default.mysql8.0"
+  publicly_accessible           = true
+  db_subnet_group_name          = module.vpc.database_subnet_group_name
+  availability_zone             = module.vpc.azs[0]
+  skip_final_snapshot           = true
+}
+
+resource "aws_db_instance" "db_order" {
+  depends_on                    = [random_password.db_order_password]
+  allocated_storage             = 5
+  db_name                       = "order"
+  engine                        = "mysql"
+  engine_version                = "8.0.34"
+  allow_major_version_upgrade   = true
+  instance_class                = "db.t3.micro"
+  username                      = "fiap"
+  password                      = random_password.db_order_password.result
   parameter_group_name          = "default.mysql8.0"
   publicly_accessible           = true
   db_subnet_group_name          = module.vpc.database_subnet_group_name
--- secrets.tf
+++ secrets.tf
@@ -1,19 +1,37 @@
-resource "aws_secretsmanager_secret" "database_burger" {
-  name = "rds_burger"
+resource "aws_secretsmanager_secret" "db_product" {
+  name = "rds_product"
 }
 
-resource "aws_secretsmanager_secret_version" "database_burger" {
+resource "aws_secretsmanager_secret_version" "db_product" {
   depends_on = [aws_db_instance.default]
   secret_id     = aws_secretsmanager_secret.database_burger.id
   secret_string = <<EOF
 {
-  "username": "${aws_db_instance.default.username}",
-  "password": "${random_password.db_master_password.result}",
-  "host": "${aws_db_instance.default.endpoint}",
-  "port": ${aws_db_instance.default.port}
+  "username": "${aws_db_instance.db_product.username}",
+  "password": "${random_password.db_product_password.result}",
+  "host": "${aws_db_instance.db_product.endpoint}",
+  "port": ${aws_db_instance.db_product.port}
 }
 EOF
 }
+
+resource "aws_secretsmanager_secret" "db_order" {
+  name = "rds_order"
+}
+
+resource "aws_secretsmanager_secret_version" "db_order" {
+  depends_on = [aws_db_instance.default]
+  secret_id     = aws_secretsmanager_secret.database_burger.id
+  secret_string = <<EOF
+{
+  "username": "${aws_db_instance.db_order.username}",
+  "password": "${random_password.db_order_password.result}",
+  "host": "${aws_db_instance.db_order.endpoint}",
+  "port": ${aws_db_instance.db_order.port}
+}
+EOF
+}
+
 
 resource "aws_secretsmanager_secret" "token_jwt" {
   name = "token_jwt"
