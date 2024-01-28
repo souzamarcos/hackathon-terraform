065a0973c5335f307f2303b1b9ca8e171a36ce51
Marcos Souza <markin.mv.amaral@gmail.com>
2024-01-28 10:41:04 -0300

    chore: Add global secondary index

--- dynamo.tf
+++ dynamo.tf
@@ -3,9 +3,18 @@ resource "aws_dynamodb_table" "tf_clients_table" {
   billing_mode = "PROVISIONED"
   read_capacity= "2"
   write_capacity= "2"
+  hash_key = "id"
+
   attribute {
     name = "id"
     type = "S"
   }
-  hash_key = "id"
+
+  global_secondary_index {
+    name               = "cpf"
+    hash_key           = "cpf"
+    write_capacity     = 2
+    read_capacity      = 2
+    projection_type    = "ALL "
+  }
 }
