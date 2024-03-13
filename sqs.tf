7c41fb7b7d0042e5ed2fdaf12c5ecbb15d1b2d14
Marcos Souza <markin.mv.amaral@gmail.com>
2024-03-13 18:35:26 -0300

    feat: Create notification queue

--- sqs.tf
+++ sqs.tf
@@ -11,3 +11,10 @@ resource "aws_sqs_queue" "order_queue" {
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
 }
+
+resource "aws_sqs_queue" "notification_queue" {
+  name                      = "notification-queue"
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
