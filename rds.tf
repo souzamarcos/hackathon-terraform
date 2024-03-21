8a9e5fbc145753213b3e5865035c2e5cc1173b88
Marcos Souza <markin.mv.amaral@gmail.com>
2024-03-21 20:10:40 -0300

    feat: Configure rds replica

--- rds.tf
+++ rds.tf
@@ -4,6 +4,12 @@ resource "random_password" "db_working_hours_password"{
   override_special = "_!%^"
 }
 
+resource "random_password" "db_working_hours_replica_password"{
+  length           = 16
+  special          = true
+  override_special = "_!%^"
+}
+
 resource "aws_db_instance" "db_working_hours" {
   depends_on                    = [random_password.db_working_hours_password]
   allocated_storage             = 5
@@ -20,4 +26,17 @@ resource "aws_db_instance" "db_working_hours" {
   db_subnet_group_name          = module.vpc.database_subnet_group_name
   availability_zone             = module.vpc.azs[0]
   skip_final_snapshot           = true
+}
+
+resource "aws_db_instance" "db_working_hours_replica" {
+  instance_class                = "db.t3.micro"
+  skip_final_snapshot           = true
+  backup_retention_period       = 7
+  replicate_source_db           = aws_db_instance.db_working_hours.identifier
+  username                      = "fiap"
+  parameter_group_name          = "default.mysql8.0"
+  publicly_accessible           = true
+  password                      = random_password.db_working_hours_replica_password.result
+  db_subnet_group_name          = module.vpc.database_subnet_group_name
+  availability_zone             = module.vpc.azs[0]
 }
